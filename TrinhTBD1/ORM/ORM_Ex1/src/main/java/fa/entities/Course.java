299b6c26decfcb1812d1b93a870f3b13ad5f81ec
TrinhTBD1 <trinhtbd1@fsoft.com.vn>
2026-05-27 11:18:50 +0700

    Add ORM_Ex1 project

---
+++ TrinhTBD1/ORM/ORM_Ex1/src/main/java/fa/entities/Course.java
@@ -0,0 +1,88 @@
+package fa.entities;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(nullable = false)
+    private int credit;
+
+    @ManyToMany(mappedBy = "courses", fetch = FetchType.LAZY)
+    private Set<Student> students = new HashSet<>();
+
+    public Course() {
+    }
+
+    public Course(String title, int credit) {
+        this.title = title;
+        this.credit = credit;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Course{id=").append(id)
+          .append(", title='").append(title).append('\'')
+          .append(", credit=").append(credit)
+          .append("}");
+        return sb.toString();
+    }
+}
