aeafcfb8afe8d47d67c11e419cd422492924d171
MixterTom <nttltommy2000@gmail.com>
2026-05-29 11:20:31 +0700

    feat: initialize Hibernate project with Employee entity

---
+++ LongNTT8/hibernate_assignment01/src/main/java/fa/training/entities/Employee.java
@@ -0,0 +1,63 @@
+package fa.training.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "EMPLOYEE")
+public class Employee {
+
+    @Id
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "First_Name", length = 50, nullable = false)
+    private String firstName;
+
+    @Column(name = "Last_Name", length = 50, nullable = false)
+    private String lastName;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
