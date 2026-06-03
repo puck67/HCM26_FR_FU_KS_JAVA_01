622c027f562ce2916d1254d8349862dea19f49a9
kietKS <kietnase185061@fpt.edu.vn>
2026-06-03 15:52:46 +0700

    asm 3

--- KietNA13/FEE/ASM3/src/main/java/com/lms/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.lms.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.lms")
-public class AppConfig {
-}
