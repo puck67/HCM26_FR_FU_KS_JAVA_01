5750bc377e568d1be67eab2180fdd398b6cf489d
thanghung18062003 <hungpltse172380@fpt.edu.vn>
2026-06-17 15:38:19 +0700

    submit ASM2 spring

---
+++ HungPLT1/Spring/ASM2/src/main/java/fa/training/jsfw_s_a102/JsfwSA102Application.java
@@ -0,0 +1,13 @@
+package fa.training.jsfw_s_a102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsfwSA102Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsfwSA102Application.class, args);
+    }
+
+}
