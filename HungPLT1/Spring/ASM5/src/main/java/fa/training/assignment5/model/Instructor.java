81d6fdfd26b1c878ef8087c1d2a0bcf701adfa30
thanghung18062003 <hungpltse172380@fpt.edu.vn>
2026-06-18 09:36:34 +0700

    submit ASM5 spring

---
+++ HungPLT1/Spring/ASM5/src/main/java/fa/training/assignment5/model/Instructor.java
@@ -0,0 +1,37 @@
+package fa.training.assignment5.model;
+
+public class Instructor extends LmsUser {
+    private static final long serialVersionUID = 1L;
+
+    private String department;
+    private String bio;
+
+    public Instructor() {}
+
+    public Instructor(long userId, String name, String email, String department, String bio) {
+        super(userId, name, email);
+        this.department = department;
+        this.bio = bio;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    @Override
+    public void printInfo() {
+        System.out.println("Instructor [ID=" + getUserId() + ", Name=" + getName() + ", Email=" + getEmail() + ", Dept=" + department + ", Bio=" + bio + "]");
+    }
+}
