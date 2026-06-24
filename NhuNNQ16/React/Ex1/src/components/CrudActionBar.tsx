cf652d7d6e490690da12da798de2ccc65846e987
Quỳnh Như <nnquynhnhu49@gmail.com>
2026-06-24 11:19:33 +0700

    submit Ex1 React

---
+++ NhuNNQ16/React/Ex1/src/components/CrudActionBar.tsx
@@ -0,0 +1,24 @@
+import { ButtonConfig } from './ButtonList';
+import { ButtonList } from './ButtonList';
+
+export function CrudActionBar({
+    onAdd,
+    onImport,
+    onExport
+}: {
+    onAdd?: () => void;
+    onImport?: () => void;
+    onExport?: () => void;
+}) {
+    const buttons: ButtonConfig[] = [
+        ...(onAdd ? [{ title: "Add Student", action: onAdd, style: "btn btn-primary", icon: "+" }] : []),
+        ...(onImport ? [{ title: "Import", action: onImport, style: "btn btn-outline", icon: "📥" }] : []),
+        ...(onExport ? [{ title: "Export", action: onExport, style: "btn btn-outline", icon: "📤" }] : []),
+    ];
+
+    return (
+        <div className="crud-action-bar">
+            <ButtonList items={buttons} />
+        </div>
+    );
+}
