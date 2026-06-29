7ab5502ceb19d7b2c0ac62e5cfa54e3df6a835b4
thanghung18062003 <hungpltse172380@fpt.edu.vn>
2026-06-29 16:22:09 +0700

    update ASM2

--- HungPLT1/React/ASM2/REACT_Assignment02/ex1_task1/src/lib/tasksRepo.ts
+++
@@ -1,4 +0,0 @@
-import { JsonRepository } from './jsonRepository';
-import { Task } from '@/types';
-
-export const tasksRepo = new JsonRepository<Task>('tasks.json');
--- HungPLT1/React/ASM2/REACT_Assignment02/ex1_task1/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-export interface Task {
-  id: string;
-  name: string;
-  description: string;
-  completed: boolean;
-  createdAt: string;
-  updatedAt: string;
-}
